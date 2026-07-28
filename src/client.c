cd494c86781e91826e3df6534159f02e31b07d9a
Jinglin Zhang <jinglin_zhang_2@sfu.ca>
2026-07-27 19:47:11 -0700

    client.c saved

--- src/client.c
+++ src/client.c
@@ -1,4 +1,19 @@
 #include "client.h"
+#include "helper.h"
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
-int main() { return 0; }
+int main(int argc, char *argv[]) {
+  if (argc != 5)
+    handle_error("incorrect args");
+
+  uint32_t ip = argv[1];
+  uint16_t port = argv[2];
+
+  return 0;
+}
