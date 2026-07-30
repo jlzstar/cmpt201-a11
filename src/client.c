98f4546cfb241f80b637fd9003bc023e5f06172b
Jinglin Zhang <jinglin_zhang_2@sfu.ca>
2026-07-30 14:47:04 -0700

    client.c sender_thread()

--- src/client.c
+++ src/client.c
@@ -1,3 +1,4 @@
+#define _DARWIN_C_SOURCE
 #include "client.h"
 #include "helper.h"
 #include <errno.h>
@@ -44,57 +45,77 @@ int convert_b2str(uint8_t *buf, ssize_t buf_size, char *str, ssize_t str_size) {
 void *sender_thread(void *args) {
   client_t *c = (client_t *)args;
 
+  // generate n msgs
   for (int i = 0; i < c->num_msgs; i++) {
-
-    uint8_t buf[10];
-    char str[10 * 2 + 1];
-    getentropy(buf, 10);
-    if (convert(buf, sizeof(buf), str, sizeof(str)) != 0)
+    int byte = 32;
+    uint8_t buf[byte];
+    char str[byte * 2 + 1];
+    if (getentropy(buf, sizeof(buf)) != 0)
+      handle_error("getentropy");
+    if (convert_b2str(buf, sizeof(buf), str, sizeof(str)) != 0)
       handle_error("convert");
+
+    // build the message protocal client to server [type][data][\n]
+    uint8_t out_msg[1 + sizeof(str) + 1];
+
+    out_msg[0] = 0;
+    memcpy(out_msg + 1, str, sizeof(str));
+    size_t out_msg_len = 1 + strlen(str);
+    out_msg[out_msg_len] = '\n';
+
+    // send the byte to server one by one
+    ssize_t total_msg_sent = 0;
+    while (total_msg_sent < out_msg_len) {
+      ssize_t n = write(c->sfd, out_msg + total_msg_sent, )
+    }
+
+    // send a type 1 msg after sending all the type 0 msgs
+    uint8_t end_msg[2] = {'1', '\n'};
+    write(c->sfd, end_msg, sizeof(end_msg));
+    c->active = false;
+    return NULL;
   }
-  return 0;
-}
 
-void *receiver_thread(void *args) {}
+  void *receiver_thread(void *args) {}
 
-int main(int argc, char *argv[]) {
+  int main(int argc, char *argv[]) {
 
-  if (argc != 5)
-    handle_error("incorrect args");
+    if (argc != 5)
+      handle_error("incorrect args");
 
-  struct sockaddr_in addr;
-  ssize_t num_read;
-  char buf[BUF_SIZE];
-  int sfd;
+    struct sockaddr_in addr;
+    ssize_t num_read;
+    char buf[BUF_SIZE];
+    int sfd;
 
-  sfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sfd == -1)
-    handle_error("socket");
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1)
+      handle_error("socket");
 
-  memset(&addr, 0, sizeof(struct sockaddr_in));
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-  if (inet_pton(AF_INET, argv[2], &addr.sin_addr) <= 0)
-    handle_error("inet_pton");
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, argv[2], &addr.sin_addr) <= 0)
+      handle_error("inet_pton");
 
-  if (connect(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1)
-    handle_error("connect");
+    if (connect(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1)
+      handle_error("connect");
 
-  client_t c;
-  c.sfd = sfd;
-  c.num_msgs = (uint8_t)atoi(argv[3]);
-  c.log_fp = fopen(argv[4], "w");
-  c.active = false;
+    client_t c;
+    c.sfd = sfd;
+    c.num_msgs = (uint8_t)atoi(argv[3]);
+    c.log_fp = fopen(argv[4], "w");
+    c.active = false;
 
-  pthread_t sender_tid, receiver_tid;
-  pthread_create(&sender_tid, NULL, sender_thread, &c);
-  pthread_create(&receiver_tid, NULL, receiver_thread, &c);
+    pthread_t sender_tid, receiver_tid;
+    pthread_create(&sender_tid, NULL, sender_thread, &c);
+    pthread_create(&receiver_tid, NULL, receiver_thread, &c);
 
-  pthread_join(sender_tid, NULL);
-  pthread_join(receiver_tid, NULL);
+    pthread_join(sender_tid, NULL);
+    pthread_join(receiver_tid, NULL);
 
-  fclose(c.log_fp);
-  close(c.sfd);
+    fclose(c.log_fp);
+    close(c.sfd);
 
-  return 0;
-}
+    return 0;
+  }
