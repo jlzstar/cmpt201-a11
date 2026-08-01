a4aacbb227b0e56c21793bf136db688c22c1d694
Jinglin Zhang <jinglin_zhang_2@sfu.ca>
2026-07-31 18:23:13 -0700

    server.c fixes

--- src/server.c
+++ src/server.c
@@ -25,6 +25,15 @@ typedef struct {
   uint32_t count;
 } MsgHandle_t;
 
+typedef struct {
+  int sfd;
+  int32_t ip;
+  int16_t port;
+  bool active;
+  uint8_t buf[BUF_SIZE];
+  size_t buf_len;
+} client_t;
+
 int init_server_socket(int16_t port, int backlog) {
   struct sockaddr_in addr;
 
@@ -44,6 +53,13 @@ int init_server_socket(int16_t port, int backlog) {
   return sfd;
 }
 
+void send_type0_msg() { return; }
+
+void send_type1_msg() { return; }
+
+static int num_clients_active = 0;
+static int num_clients_done = 0;
+
 int main(int argc, char *argv[]) {
   if (argc != 3) {
     handle_error("incorrect arguments");
@@ -53,6 +69,8 @@ int main(int argc, char *argv[]) {
   if (NUM_CLIENTS == 0) {
     handle_error("# clients is 0");
   }
+
+  client_t clients[NUM_CLIENTS];
 
   struct sockaddr_in remote_addr;
   int sfd, cfd, epollfd;
@@ -86,6 +104,8 @@ int main(int argc, char *argv[]) {
         if (cfd == -1)
           handle_error("accept");
 
+      } else {
+        // set up non-blocking
         int flags = fcntl(cfd, F_GETFL, 0);
         if (flags == -1)
           handle_error("fcntl");
@@ -98,17 +118,25 @@ int main(int argc, char *argv[]) {
         if (epoll_ctl(epollfd, EPOLL_CTL_ADD, cfd, &ev) == -1)
           handle_error("epoll_ctl: conn_sock");
 
-      } else {
-        printf("client connected!\n");
-        while ((num_read = read(events[i].data.fd, buf, BUF_SIZE)) > 0) {
-          if (write(STDOUT_FILENO, buf, num_read) != num_read)
-            handle_error("write");
-          if (num_read == -1)
-            handle_error("read");
-        }
+        // initialize clients
+        clients[i].active = true;
+        clients[i].sfd = cfd;
+        clients[i].ip = remote_addr.sin_addr.s_addr;
+        clients[i].port = remote_addr.sin_port;
+        clients[i].buf_len = 0;
+      }
+
+      printf("client connected!\n");
+      while ((num_read = read(events[i].data.fd, buf, BUF_SIZE)) > 0) {
+
+        if (write(events[i].data.fd, buf, num_read) != num_read)
+          handle_error("write");
+        if (num_read == -1)
+          handle_error("read");
       }
     }
   }
+}
 
-  return 0;
+return 0;
 }
