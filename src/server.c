1ca2b9c0637dc46ecc5910057c4db9c6b1b7b7c1
Jinglin Zhang <jinglin_zhang_2@sfu.ca>
2026-08-03 20:37:12 -0700

    saved work

--- src/server.c
+++ src/server.c
@@ -17,7 +17,8 @@
 
 pthread_mutex_t clientLock;
 pthread_mutex_t clientLock2;
-pthread static int num_clients_connected = 0;
+pthread_mutex_t num_clientLock;
+static int num_clients_connected = 0;
 static int num_type1_received = 0;
 
 typedef struct {
@@ -79,159 +80,180 @@ size_t s2c_msging_protocol(uint8_t *client_buf, size_t client_buf_len, uint32_t 
 bool handle_client_msg(client_t *clients, uint8_t num_clients, int sender_index, uint8_t *read_buf,
                        ssize_t num_read) {
 
-  int type = read_buf[0];
-  if (type == 0) {
-    char out_buf[1 + 4 + 2 + BUF_SIZE + 1];
-    size_t out_len = s2c_msging_protocol(read_buf, num_read, &clients[sender_index].ip,
-                                         clients[sender_index].port, out_buf);
-    for (int i = 0; i < num_clients; i++) {
-      if (clients[i].active == true)
-        write(clients[i].sfd, out_buf, out_len);
-    }
-    return false; // server does not terminate
-
-  } else if (type == 1) {
-    // clients[sender_index].active = false;
-
-    num_type1_received++;
-    uint8_t end = '1';
-    write(clients[sender_index].sfd, &end, 1);
-  }
-
-  if (num_type1_received >= num_clients_connected) {
-    // send type 1 msg to all clients
-    uint8_t end = 1;
-    for (int i = 0; i < num_clients; i++) {
-      if (clients[i].active == true) {
-        write(clients[i].sfd, &end, 1);
-      }
-    }
-    return true; // server should terminate
-  }
-  return false;
-}
-
-int find_client_index_by_fd(client_t *clients, int num_clients, int fd) {
-  for (int i = 0; i < num_clients; i++) {
-    if (clients[i].sfd == fd && clients[i].active == true)
-      return i;
-  }
-  return -1;
-}
-
-int find_free_slot(client_t *clients, int n_cl) {
-  for (int i = 0; i < n_cl; i++) {
-    if (clients[i].active == false)
-      return i;
-  }
-  return -1;
-}
-
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    handle_error("incorrect arguments");
-  }
-  uint16_t port = (uint16_t)atoi(argv[1]);
-  const uint8_t NUM_CLIENTS = atoi(argv[2]);
-  if (NUM_CLIENTS == 0) {
-    handle_error("# clients is 0");
-  }
-
-  client_t clients[NUM_CLIENTS];
-  memset(clients, 0, sizeof(clients));
-  struct sockaddr_in client_addr;
-  int sfd, cfd, epollfd;
-  int nfds;
-  socklen_t addrlen = sizeof(struct sockaddr_in);
-  uint8_t read_buf[BUF_SIZE];
-  struct epoll_event ev, events[NUM_CLIENTS];
-
-  sfd = init_server_socket(port, NUM_CLIENTS);
-
-  // 1. create epoll
-  epollfd = epoll_create1(0);
-  if (epollfd == -1)
-    handle_error("epoll_create1");
-
-  // 2.
-  ev.events = EPOLLIN | EPOLLOUT;
-  ev.data.fd = sfd;
-  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sfd, &ev) == -1)
-    handle_error("epoll_ctl");
-
+  //
   for (;;) {
-    nfds = epoll_wait(epollfd, events, NUM_CLIENTS, -1);
-    if (nfds == -1)
-      handle_error("epoll_wait");
+    // check for msg in read_buf
+    uint8_t *newLine = NULL;
 
-    for (int i = 0; i < nfds; ++i) {
-      // case 1: the incoming signal is a new client trying to connect
-      if (events[i].data.fd == sfd) {
-        memset(&client_addr, 0, sizeof(struct sockaddr_in));
-        cfd = accept(sfd, (struct sockaddr *)&client_addr, &addrlen);
-        if (cfd == -1)
-          handle_error("accept");
+    // if not a complete msg, read from socket for more bytes
+    if (newLine == NULL) {
+      ssize_t n = read();
+    }
 
-        // set up non-blocking
-        int flags = fcntl(cfd, F_GETFL, 0);
-        if (flags == -1)
-          handle_error("fcntl");
-        flags |= O_NONBLOCK;
-        if (fcntl(cfd, F_SETFL, flags) == -1)
-          handle_error("fcntl");
+    // type 1 msg
+    // increment type1 received
+    // write type 1 msg to the sender
 
-        ev.events = EPOLLIN;
-        ev.data.fd = cfd;
+    // type 0 msg - send it to all clients (including sender)
+    // parse msg into [type][ip][port][msg][\n] format
+    // send it
 
-        // add cfd into epoll watchlist
-        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, cfd, &ev) == -1)
-          handle_error("epoll_ctl: conn_sock");
+    // check if server should terminate
 
-        int indx = find_free_slot(clients, NUM_CLIENTS);
-        if (indx == -1) {
-          close(cfd);
-        } else {
-          clients[indx].sfd = cfd;
-          clients[indx].active = true;
-          clients[indx].ip = client_addr.sin_addr.s_addr;
-          clients[indx].port = client_addr.sin_port;
-          num_clients_connected++;
+    int type = read_buf[0];
+    if (type == 0) {
+      char out_buf[1 + 4 + 2 + BUF_SIZE + 1];
+      size_t out_len = s2c_msging_protocol(read_buf, num_read, &clients[sender_index].ip,
+                                           clients[sender_index].port, out_buf);
+      for (int i = 0; i < num_clients; i++) {
+        if (clients[i].active == true)
+          write(clients[i].sfd, out_buf, out_len);
+      }
+      return false; // server does not terminate
+
+    } else if (type == 1) {
+      // clients[sender_index].active = false;
+
+      num_type1_received++;
+      uint8_t end = 1;
+      write(clients[sender_index].sfd, &end, 1);
+    }
+
+    if (num_type1_received >= num_clients_connected) {
+      // send type 1 msg to all clients
+
+      uint8_t end = 1;
+      for (int i = 0; i < num_clients; i++) {
+        if (clients[i].active == true) {
+          write(clients[i].sfd, &end, 1);
         }
+      }
+      return true; // server should terminate
+    }
+    return false;
+  }
 
-      } else {
-        // case 2: the incoming signal is an existing client sending data
+  int find_client_index_by_fd(client_t * clients, int num_clients, int fd) {
+    for (int i = 0; i < num_clients; i++) {
+      if (clients[i].sfd == fd && clients[i].active == true)
+        return i;
+    }
+    return -1;
+  }
 
-        int indx = find_client_index_by_fd(clients, NUM_CLIENTS, events[i].data.fd);
-        if (indx == -1)
+  int find_free_slot(client_t * clients, int n_cl) {
+    for (int i = 0; i < n_cl; i++) {
+      if (clients[i].active == false)
+        return i;
+    }
+    return -1;
+  }
 
-          continue;
+  int main(int argc, char *argv[]) {
+    if (argc != 3) {
+      handle_error("incorrect arguments");
+    }
+    uint16_t port = (uint16_t)atoi(argv[1]);
+    const uint8_t NUM_CLIENTS = atoi(argv[2]);
+    if (NUM_CLIENTS == 0) {
+      handle_error("# clients is 0");
+    }
 
-        ssize_t num_read = 0;
-        while ((num_read = read(events[i].data.fd, read_buf, BUF_SIZE)) > 0) {
+    client_t clients[NUM_CLIENTS];
+    memset(clients, 0, sizeof(clients));
+    struct sockaddr_in client_addr;
+    int sfd, cfd, epollfd;
+    int nfds;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    uint8_t read_buf[BUF_SIZE];
+    struct epoll_event ev, events[NUM_CLIENTS];
 
-          // printf("client connected from ip: %s, port: %u\n", ip_str, port_c);
+    sfd = init_server_socket(port, NUM_CLIENTS);
 
-          // send the incoming msg to ALL clients (including sender)
+    // 1. create epoll
+    epollfd = epoll_create1(0);
+    if (epollfd == -1)
+      handle_error("epoll_create1");
 
-          memcpy(clients[indx].buf, read_buf, num_read);
-          clients[indx].buf_len = num_read;
-          bool term = handle_client_msg(clients, NUM_CLIENTS, indx, clients[indx].buf,
-                                        clients[indx].buf_len);
-          if (term == true) {
-            close_all_sfd(clients, NUM_CLIENTS);
-            close(sfd);
-            printf("server terminates successfully");
-            return 0;
+    // 2.
+    ev.events = EPOLLIN | EPOLLOUT;
+    ev.data.fd = sfd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sfd, &ev) == -1)
+      handle_error("epoll_ctl");
+
+    for (;;) {
+      nfds = epoll_wait(epollfd, events, NUM_CLIENTS, -1);
+      if (nfds == -1)
+        handle_error("epoll_wait");
+
+      for (int i = 0; i < nfds; ++i) {
+        // case 1: the incoming signal is a new client trying to connect
+        if (events[i].data.fd == sfd) {
+          memset(&client_addr, 0, sizeof(struct sockaddr_in));
+          cfd = accept(sfd, (struct sockaddr *)&client_addr, &addrlen);
+          if (cfd == -1)
+            handle_error("accept");
+
+          // set up non-blocking
+          int flags = fcntl(cfd, F_GETFL, 0);
+          if (flags == -1)
+            handle_error("fcntl");
+          flags |= O_NONBLOCK;
+          if (fcntl(cfd, F_SETFL, flags) == -1)
+            handle_error("fcntl");
+
+          ev.events = EPOLLIN;
+          ev.data.fd = cfd;
+
+          // add cfd into epoll watchlist
+          if (epoll_ctl(epollfd, EPOLL_CTL_ADD, cfd, &ev) == -1)
+            handle_error("epoll_ctl: conn_sock");
+
+          int indx = find_free_slot(clients, NUM_CLIENTS);
+          if (indx == -1) {
+            close(cfd);
+          } else {
+            clients[indx].sfd = cfd;
+            clients[indx].active = true;
+            clients[indx].ip = client_addr.sin_addr.s_addr;
+            clients[indx].port = client_addr.sin_port;
+            num_clients_connected++;
+          }
+
+        } else {
+          // case 2: the incoming signal is an existing client sending data
+
+          int indx = find_client_index_by_fd(clients, NUM_CLIENTS, events[i].data.fd);
+          if (indx == -1)
+
+            continue;
+
+          ssize_t num_read = 0;
+          while ((num_read = read(events[i].data.fd, read_buf, BUF_SIZE)) > 0) {
+
+            // printf("client connected from ip: %s, port: %u\n", ip_str, port_c);
+
+            // send the incoming msg to ALL clients (including sender)
+
+            memcpy(clients[indx].buf, read_buf, num_read);
+            clients[indx].buf_len = num_read;
+            bool term = handle_client_msg(clients, NUM_CLIENTS, indx, clients[indx].buf,
+                                          clients[indx].buf_len);
+            if (term == true) {
+              close_all_sfd(clients, NUM_CLIENTS);
+              close(sfd);
+              printf("server terminates successfully");
+              return 0;
+            }
+          }
+          if (num_read == 0 && !(num_read == -1 && errno == EAGAIN && errno == EWOULDBLOCK)) {
+            // client disconnects
+            epoll_ctl(epollfd, EPOLL_CTL_DEL, clients[indx].sfd, NULL);
+            close(clients[indx].sfd);
+            clients[indx].active = false;
           }
         }
-        if (num_read == 0 && !(num_read == -1 && errno == EAGAIN && errno == EWOULDBLOCK)) {
-          // client disconnects
-          epoll_ctl(epollfd, EPOLL_CTL_DEL, clients[indx].sfd, NULL);
-          close(clients[indx].sfd);
-          clients[indx].active = false;
-        }
       }
     }
+    return 0;
   }
-  return 0;
-}
